390ade1e293e41bd7c2ddd1666c41eedb84c80db
Pedro Coelho <pmnco1@iscte-iul.pt>
2026-03-28 10:46:30 +0000

    Esquema da T2

--- src/main/scala/GameLogic.scala
+++ src/main/scala/GameLogic.scala
@@ -6,4 +6,11 @@ object GameLogic {
     /*def randomMove(lstOpenCoords: List[Coord2D], rand: MyRandom): (Coord2D, MyRandom) = {
         
     }*/
+
+
+    // T2: Move a peça e devolve o tabuleiro e a lista das pos válidas
+    /*def play(board:Board, player: Stone, coordFrom:Coord2D,coordTo:Coord2D,lstOpenCoords:List[Coord2D]):(Option[Board],List[Coord2D]):(Option[Board], List[Coord2D]) {
+
+
+    } */
 }
