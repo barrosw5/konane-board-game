cd03b01d99c7e05aa15d3cc263b1757c8ed8c37a
João Almeida <jfbalmeida10@gmail.com>
2026-04-13 12:43:31 +0100

    Funciona perfeitamente no meu computador :)

--- src/main/scala/GameLogic.scala
+++ src/main/scala/GameLogic.scala
@@ -181,19 +181,6 @@ object GameLogic {
         }
     }
     
-    
-    
-    // T3: Realizar uma jogada aleatória
-    /*def playRandomly(board:Board,
-                    r:MyRandom,
-                    player:Stone,
-                    lstOpenCoords:List[Coord2D],
-                    f:(List[Coord2D],MyRandom)=>(Coord2D,MyRandom)):
-                    (Option[Board],MyRandom,List[Coord2D],Option[Coord2D]) = {
-        val (coord, nextR) = f(lstOpenCoords,MyRandom)
-        play(coord)
-    }*/
-    
     def playRandomly(board: Board,
                      r: MyRandom,
                      player: Stone,
