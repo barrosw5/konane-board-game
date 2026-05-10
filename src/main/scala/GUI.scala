83e6ea058d3aa28f7ff2744808ec6359043ea780
jfbalmeida <jfbalmeida10@gmail.com>
2026-05-10 13:47:25 +0100

    Arranque de GUI
    falta controller e a scene em si.

--- src/main/scala/GUI.scala
+++ src/main/scala/GUI.scala
@@ -1,6 +1,27 @@
 // Graphical User Interface
 // Fazer aqui T6, T8
+import javafx.application.Application
+import javafx.fxml.FXMLLoader
+import javafx.scene.{Parent, Scene}
+import javafx.stage.Stage
 
-class GUI {
+class GUI extends Application {
+  override def start(primaryStage: Stage): Unit = {
+    primaryStage.setTitle("Konane")
 
+    // Carrega o layout desenhado
+    val fxmlLoader = new FXMLLoader(getClass.getResource("Konane.fxml"))
+    val mainViewRoot: Parent = fxmlLoader.load()
+
+    val scene = new Scene(mainViewRoot)
+    primaryStage.setScene(scene)
+    primaryStage.show()
+  }
+}
+
+// Para arrancar a interface
+object GUIApp {
+  def main(args: Array[String]): Unit = {
+    Application.launch(classOf[KonaneGUI], args: _*)
+  }
 }
